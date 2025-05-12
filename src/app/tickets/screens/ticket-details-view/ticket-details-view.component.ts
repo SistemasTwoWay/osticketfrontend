825e4b7d87527d3fdda50149b223a7c09dc51054
AlfonsoAD <alfonsoad097@gmail.com>
2025-05-12 18:01:13 -0500

    OTWT-12 feat: enhance ticket management with notification service and form validation improvements

--- src/app/tickets/screens/ticket-details-view/ticket-details-view.component.ts
+++ src/app/tickets/screens/ticket-details-view/ticket-details-view.component.ts
@@ -1,23 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NzCardModule } from 'ng-zorro-antd/card';
-import { NzDescriptionsModule, NzDescriptionsSize } from 'ng-zorro-antd/descriptions'
-import { TicketResume } from '../../../domain/tickets/domain/ticket-resume.model';
+import {
+  NzDescriptionsModule,
+  NzDescriptionsSize,
+} from 'ng-zorro-antd/descriptions';
+import { TicketLite } from '../../../domain/tickets/domain/ticket-resume.model';
 import { CommonModule } from '@angular/common';
-import { getPriorityClass, getStatusClass } from '../../../shared/helpers/color-tickets';
+import {
+  getPriorityClass,
+  getStatusClass,
+} from '../../../shared/helpers/color-tickets';
 import { TicketDataDescriptionComponent } from '../../components/ticket-data-description/ticket-data-description.component';
 
 @Component({
   selector: 'app-ticket-details-view',
-  imports: [
-    TicketDataDescriptionComponent,
-  ],
+  imports: [TicketDataDescriptionComponent],
   templateUrl: './ticket-details-view.component.html',
-  styleUrl: './ticket-details-view.component.css'
+  styleUrl: './ticket-details-view.component.css',
 })
 export class TicketDetailsViewComponent implements OnInit {
   number!: string;
-  ticket!: TicketResume;
+  ticket!: TicketLite;
 
   constructor(private _route: ActivatedRoute) {}
 
